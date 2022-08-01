c08852c4642de1520e0cf06768d130f1331761b1
Madina <madiinaamiirova@gmail.com>
2022-08-02 03:42:15 +0400

    commit

---
+++ Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    class Student : Person
+    {
+        private static int _id;
+        public int Id { get; }
+        public string[] Lessons { get; set; } = new string[0];
+
+        public Student()
+        {
+            _id++;
+            Id = _id;
+        }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    class Teacher : Person
+    {
+        private static int _id;
+        private double _experience;
+        public int Id { get; }
+        public double Experience
+        {
+            get => _experience;
+            set
+            {
+                if (value > 0 && value <= 80)
+                {
+                    _experience = value;
+                }
+                else throw new Exception();
+            }
+        }
+        public Teacher()
+        {
+            _id++;
+            Id = _id;
+        }
+
+
+        public void TeacherInfo()
+        {
+            Console.WriteLine($@"Id -->{Id}
+            Experience --> {Experience}");
+        }
+    }
+}
